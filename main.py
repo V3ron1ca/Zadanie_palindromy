739b1b24b70a6a84c8586e19ee4fdb321945b2dc
Weronika Win <w1nw3r25@gmail.com>
2021-03-27 12:24:10 +0100

    Add documentation

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 def palindromy(x):
+    """
+    tworzymy funkcje "palindoromy",
+    wpisujemy zmienną l oznaczającą długość podanego słowa
+    tworzymy pętle rozczytującą wyraz od początku a potem od końca
+    jeśli słowo czytane od początku nie jest takie samo jak czytane od końca to wyskakuje return false
+    jeśli jest takie samo to return true
+    zmienna slowo przechowuje dany wyraz który nastepnie wprowadzony jest do funkcji
+    jesli podany wyraz zgadza sie z założeniem funkcji wyskakuje "Tak"
+    natomiast jak wyraz nie jest rozpoznany jako palindrom to dostajemy informacje "Nie"
+    :param x:
+    :return:
+    """
     l = len(x)
     for p in range (l-1):
         if x[p] != x[l-1-p]:
