b6d3fa04fb851b29f20d2c1bc9f3bc70ca1e4c25
V3ron1ca <81174313+V3ron1ca@users.noreply.github.com>
2021-03-29 23:00:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 def palindromy(x):
-    """
-    tworzymy funkcje "palindoromy",
-    wpisujemy zmienną l oznaczającą długość podanego słowa
-    tworzymy pętle rozczytującą wyraz od początku a potem od końca
-    jeśli słowo czytane od początku nie jest takie samo jak czytane od końca to wyskakuje return false
-    jeśli jest takie samo to return true
-    zmienna slowo przechowuje dany wyraz który nastepnie wprowadzony jest do funkcji
-    jesli podany wyraz zgadza sie z założeniem funkcji wyskakuje "Tak"
-    natomiast jak wyraz nie jest rozpoznany jako palindrom to dostajemy informacje "Nie"
-    """
-    l = len(x)
-    for p in range (l-1):
-        if x[p] != x[l-1-p]:
-            return False
-    return True
-slowo = "potop"
+    x = x.lower()
+    if x == x[::-1]:
+        return True
+    return False
+slowo = "Potop"
 funkcja = palindromy(slowo)
 if (funkcja):
     print("Tak")
